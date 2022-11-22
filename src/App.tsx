d1c5bb59961354c7f817feff696571921131da21
Coovi Meha <43079588+Afronut@users.noreply.github.com>
2022-11-21 20:16:52 -0500

    center form, add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-
-import './App.css';
-import Reserve from './pages/Reserve';
-import Users from './pages/Users';
-import Admin from './pages/Admin';
-import NotFound from './pages/NotFound';
-
+import "./App.css";
+import Reserve from "./pages/Reserve";
+import Users from "./pages/Users";
+import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-    {/* <Admin/> */}
-     {/* <Reserve/> */}
-     <Users/>
-     {/* <NotFound/> */}
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      {/* <Admin/> */}
+      {/* <Reserve/> */}
+      <Users />
+      {/* <NotFound/> */}
     </div>
   );
 }
